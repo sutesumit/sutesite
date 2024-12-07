05585453f921f1b0dc04dab883b3e72650f5f8ac
sutesumit <sumitsute9@gmail.com>
2024-12-07 18:16:19 +0530

    first commit with plain text.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Sorry for the dust—our site is under construction!</h3>
+      <p className="read-the-docs">
+        <a href="https://sumitsute.cargo.site/">Wondering what came before? See the older site version!</a>
+      </p>
+    </>
+  )
+}
+
+export default App
